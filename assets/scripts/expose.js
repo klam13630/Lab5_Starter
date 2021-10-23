42249ca56e8183bba1c52facfb0e182257484f09
Kevin Lam <klam13630@gmail.com>
2021-10-23 15:47:45 -0700

    fixed confetti

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,7 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
-
+const jsConfetti = new JSConfetti()
 function init() {
 
   var selector = document.getElementById("horn-select");
@@ -34,7 +34,6 @@ function init() {
   button.addEventListener('click', function() {
     document.getElementsByClassName("hidden")[0].play();
     if (selector.value === "party-horn" && document.getElementsByClassName("hidden")[0].volume != 0) {
-      const jsConfetti = new JSConfetti()
       jsConfetti.addConfetti()
     }
   }) 
